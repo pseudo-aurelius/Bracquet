69af4e6bb71f12c304a828ca5da29543e157118d
pseudo-aurelius <dcurtin343@gmail.com>
2024-08-11 22:12:13 -0400

    Initial Commit

---
+++ Bracquet/BracquetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BracquetApp.swift
+//  Bracquet
+//
+//  Created by Drew Curtin on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BracquetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
