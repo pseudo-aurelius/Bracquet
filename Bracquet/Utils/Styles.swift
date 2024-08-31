9dfc4c0d8452359f5e01a89e8004e9a1c7eaf2c4
pseudo-aurelius <dcurtin343@gmail.com>
2024-08-30 22:44:58 -0400

    BRAC-3 : Add Reusable Button Component

--- Bracquet/Utils/Styles.swift
+++ Bracquet/Utils/Styles.swift
@@ -9,12 +9,12 @@
 import Foundation
 
 // Application Colors
-class BraquetColors {
-    static let pureBlack: String = "#000000"
-    static let pureWhite: String = "#FFFFFF"
-    static let backgroundWhite: String = "#EBEBEB"
-    static let primaryBlue: String = "#6699CC"
-    static let primaryGreen: String = "#A6C954"
-    static let accentGray: String = "565554"
-    static let accentBlack: String = "363B44"
+enum BraquetColors: String {
+    case pureBlack = "#000000"
+    case pureWhite = "#FFFFFF"
+    case backgroundWhite = "#EBEBEB"
+    case primaryBlue = "#6699CC"
+    case primaryGreen = "#A6C954"
+    case accentGray = "565554"
+    case accentBlack = "363B44"
 }
