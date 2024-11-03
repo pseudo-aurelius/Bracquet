eff9ecd3f7644f7f5d88768266462801b8d9cf0e
pseudo-aurelius <dcurtin343@gmail.com>
2024-11-03 17:52:04 -0500

    Add Route to Login Button

---
+++ Bracquet/Views/LoginView.swift
@@ -0,0 +1,22 @@
+//  LoginView.swift
+//  Copyright Bracquet 2024
+//
+//  This parent view will allow users to authenticate
+//  themselves using a username and password, bringing
+//  them to the home view on success.
+//
+//  Created by Drew Curtin on 9/27/24.
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login View Placeholder")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
